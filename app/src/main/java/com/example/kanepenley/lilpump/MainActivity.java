e1d8bf4769bf5735051ade8346b08693e00727b9
Kane Penley <kanepenley@kanemac.nomads.utk.edu>
2017-11-30 11:53:04 -0500

    AWW HERE IT GO

---
+++ app/src/main/java/com/example/kanepenley/lilpump/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.kanepenley.lilpump;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    FragmentPagerAdapter adapterViewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
+    }
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 3;
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        // Returns total number of pages.
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        // Returns the fragment to display for a particular page.
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return HomePageFragment.newInstance();
+                case 1:
+                    return HistoryPageFragment.newInstance();
+                case 2:
+                    return SettingsPageFragment.newInstance();
+                default:
+                    return null;
+            }
+        }
+
+        // Returns the page title for the top indicator
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Home";
+                case 1:
+                    return "History";
+                case 2:
+                    return "Settings";
+                default:
+                    return null;
+            }
+
+        }
+    }
+}
+
+
